7b07cad322e896b528e2d088bedca1870811b32c
deansher <deansherthompson@gmail.com>
2024-10-01 14:14:24 -0400

    changed `analyze` names to `choose`

--- tests/test_chat_analyzer.py
+++
@@ -1,8 +0,0 @@
-import pytest
-
-
-from llm_multiple_choice import ChatAnalyzer
-
-
-def test_chat_analyzer_analyze() -> None:
-    assert False
---
+++ tests/test_choose.py
@@ -0,0 +1,8 @@
+import pytest
+
+
+from llm_multiple_choice import choose
+
+
+def test_choose() -> None:
+    assert False
